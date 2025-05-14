28dc091bee7f2f4c0b9576e982db8568bebb5ad0
solletivishnu <solletivishnu98@gmail.com>
2025-05-14 15:49:58 +0530

    firstt

--- dockerfile
+++ dockerfile
@@ -1,4 +1,5 @@
 # Dockerfile
+
 FROM python:3.11-slim
 
 ENV PYTHONDONTWRITEBYTECODE 1
@@ -6,9 +7,25 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install Python dependencies
 COPY requirements.txt .
-RUN pip install --upgrade pip && pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy project files
 COPY . .
+RUN python manage.py migrate
+# Collect static files
 RUN python manage.py collectstatic --noinput
 
+
+# Expose port
+EXPOSE 8000
+
+# Run the app
 CMD ["gunicorn", "todo_project.wsgi:application", "--bind", "0.0.0.0:8000"]
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,11 +76,11 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
