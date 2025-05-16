a5338417ed9aed7aa30b49c200e20ec1d47e404e
solletivishnu <solletivishnu98@gmail.com>
2025-05-16 23:19:04 +0530

    app_02

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -4,18 +4,19 @@
 #docker-compose pull
 #!/bin/bash
 
+#!/bin/bash
+
 cd /home/ubuntu/django_deployment || exit 1
 
 echo "[AfterInstall] Pulling the Docker image..."
 
-# Check for image_tag.txt
+# Confirm presence of the file
 if [ ! -f image_tag.txt ]; then
-  echo "image_tag.txt not found!"
+  echo "image_tag.txt not found in /home/ubuntu/django_deployment!"
   exit 1
 fi
 
+# Read the tag and pull the image
 IMAGE_TAG=$(cat image_tag.txt)
-
 echo "Using IMAGE_TAG=$IMAGE_TAG"
-
 docker pull vishnu9/todo-app:$IMAGE_TAG
