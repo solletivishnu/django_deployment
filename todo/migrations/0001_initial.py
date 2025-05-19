8532174b6eaa12ff3927f362881c5e95e4858cea
solletivishnu <solletivishnu98@gmail.com>
2025-05-19 16:26:32 +0530

    finished

--- todo/migrations/0001_initial.py
+++ todo/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
-                ('completed', models.BooleanField(default=False)),
+                ('Finished', models.BooleanField(default=False)),
             ],
         ),
     ]
