632d6a904cbd2758ce17bbe9f4bd9135a451a29f
solletivishnu <solletivishnu98@gmail.com>
2025-05-19 14:03:40 +0530

    Add_02

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,19 +1,14 @@
-##!/bin/bash
+#!/bin/bash
 set -e
 
 echo "[BeforeInstall] Stopping existing Docker containers (if any)..."
 docker ps -q | xargs -r docker stop || true
 docker system prune -af || true
 
-echo "[BeforeInstall] Creating deployment directory..."
-mkdir -p /home/ubuntu/django_deployment
-
-echo "[BeforeInstall] Setting permissions..."
-chown -R ubuntu:ubuntu /home/ubuntu/django_deployment
-chmod -R u+rwX /home/ubuntu/django_deployment
-chmod +x /home/ubuntu/django_deployment/scripts/*.sh || true
-
 echo "[BeforeInstall] Done."
+
+
+
 
 
 #!/bin/bash
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-cd /home/ubuntu/django_deployment || exit
-echo "[Install] Pulling latest changes and dependencies"
+
+#echo "[Install] Pulling latest changes and dependencies"
 # No git pull needed here — CodeDeploy does the sync
+#!/bin/bash
+#cd /home/ubuntu/django_deployment || exit
+#!/bin/bash
+set -e
+
+echo "[Install] Setting ownership and permissions..."
+chown -R ubuntu:ubuntu /home/ubuntu/django_deployment
+chmod -R u+rwX /home/ubuntu/django_deployment
+chmod +x /home/ubuntu/django_deployment/scripts/*.sh || true
+
+echo "[Install] Done."
